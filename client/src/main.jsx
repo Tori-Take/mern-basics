c8b15b8d082ccc57a40cff202d1d6e1dfa88ce68
tori-take <toritakegm@gmail.com>
2025-11-05 02:39:47 +0900

    Bootstrapのアイコンライブラリ追加

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'react-datepicker/dist/react-datepicker.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; // この行を追加
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap本体のCSS
+import 'bootstrap-icons/font/bootstrap-icons.css'; // ★★★ Bootstrap IconsのCSSを追加 ★★★
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>,
